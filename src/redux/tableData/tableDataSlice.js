8643bbbb9132400add1e8330bc976d4fe55207d7
Nadia Khoptiuk <holineinadia@gmail.com>
2022-09-04 23:17:22 +0300

    update tableDataSlice (change logic by Immer)

--- src/redux/tableData/tableDataSlice.js
+++ src/redux/tableData/tableDataSlice.js
@@ -13,9 +13,6 @@ export const tableDataSlice = createSlice({
     addRow: (state, { payload }) => {
       // writing immutable state with using Immer
       state.push(payload);
-
-      // writing immutable state with JS native tools will look like this:
-      // return [...state, payload];
     },
     incrementAmount: (state, { payload }) => {
       const { row, idToFind } = payload;
@@ -23,23 +20,8 @@ export const tableDataSlice = createSlice({
       const neededRow = state.find(el => el.M === row);
       const neededTd = neededRow.columns.find(el => el.id === idToFind);
 
-      // writing immutable state with using Immer (for change value simplier)
+      // writing immutable state with using Immer
       neededTd.amount = neededTd.amount + 1;
-
-      // writing immutable state with JS native tools will look like this:
-
-      // const { N, amount } = neededTd;
-      // const newTd = { N, id: idToFind, amount: amount + 1 };
-      // const filteredColumns = neededRow.columns.filter(
-      //   el => el.id !== idToFind
-      // );
-
-      // const newColumns = [...filteredColumns, newTd].sort((a, b) => a.N - b.N);
-      // const filteredState = state.filter(el => el.M !== row);
-      // const newRow = { ...neededRow, columns: newColumns };
-      // const newState = [...filteredState, newRow].sort((a, b) => a.M - b.M);
-
-      // return newState;
     },
   },
 });
